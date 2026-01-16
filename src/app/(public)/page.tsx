1d601162637b5a9af34e23660bede950aa2bdec7
MIA <sohanahmed708@gmail.com>
2026-01-16 11:22:32 +0900

    first commit

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <main className="min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-blue-600">News Portal Home</h1>
+            <p className="mt-2 text-gray-600">Public site is running...</p>
+        </main>
+    );
+}
